a5d9546cb8f8721b0794877797f8bc27b34711d9
Moe Martinez <deadfocus@gmail.com>
2018-03-16 08:44:34 -0500

    New layout with some re-write of javascript to make it work

--- t1dc.js
+++ t1dc.js
@@ -1,27 +1,29 @@
 (function (window, document) {
 
-    var calculator = document.getElementById('BolusCalculator'),
-        carbIntake = document.getElementById('carbIntake'),
-        carbRatio  = document.getElementById('CarbRatio'),
-        CarbBolusTotal = document.getElementById('CarbBolusTotal'),
+    var calculator = document.getElementById('bolus-calculator'),
+        carbIntake = document.getElementById('carb-intake'),
+        carbRatio  = document.getElementById('carb-ratio'),
+        CarbBolusTotal = document.getElementById('carb-bolus-total'),
         
-        BGL = document.getElementById('BGL'),
-        correctionTarget = document.getElementById('correctionTarget'),
-        correctionAmount = document.getElementById('correctionAmount'),
-        correctionFactor = document.getElementById('correctionFactor'),
-        correctionBolusTotal = document.getElementById('correctionBolusTotal'),
-        buttonCalcCorrection = document.getElementById('buttonCalcCorrection');
+        BGL = document.getElementById('blood-glucose-level'),
+        correctionTarget = document.getElementById('correction-target'),
+        correctionAmount = document.getElementById('correction-amount'),
+        correctionFactor = document.getElementById('correction-factor'),
+        correctionBolusTotal = document.getElementById('correction-bolus-total'),
+        insulinBolusTotal = document.getElementById('insulin-bolus-total'),
+        calculateInsulinBolus = document.getElementById('calculate-insulin-bolus');
+        
+
+
+        
     
 
      
-    buttonCalcCarb.onclick = function(){
+    calculateInsulinBolus.onclick = function(){
         var cbt = (parseFloat(Number(carbIntake.value)) / parseFloat(Number(carbRatio.value))).toFixed(2);
         CarbBolusTotal.innerHTML = cbt;
-        insulinBolus();
-        return false;
-    }
 
-    buttonCalcCorrection.onclick = function(){
+
         var correctionAmountTotal = parseFloat(Number(BGL.value)) - parseFloat(Number(correctionTarget.value));
         correctionAmount.value = correctionAmountTotal;
         
